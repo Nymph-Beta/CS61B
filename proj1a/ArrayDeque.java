1863f0a4155f8ac3520ab1360070b9fc9158ea88
Nymph-Beta <yuyu82438@gmail.com>
2025-08-08 09:24:36 +0800

    feat(project1a): 更新 ArrayDeque / LinkedListDeque & 测试

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,4 +1,186 @@
-package PACKAGE_NAME;
+public class ArrayDeque<T> {
+    private T[] items;
+    private int size;
 
-public class ArrayDeque {
+    private int head;
+    private int tail;
+
+    private static final int INITIAL_CAPACITY = 8;
+    private static final int RESIZE_FACTOR = 2;
+    private static final double MIN_USAGE_RATIO = 0.25;
+
+    public ArrayDeque() {
+        items = (T[]) new Object[INITIAL_CAPACITY];
+        size = 0;
+        head = 0;
+        tail = 0;
+    }
+
+    /**
+     *
+     * @param index
+     * @return
+     */
+    private int getNext(int index){
+        return (index + 1) % items.length;
+    }
+
+    /**
+     * 计算给定索引在循环数组中的上一个位置。
+     * @param index
+     * @return
+     */
+    private int getPrev(int index){
+        return(index - 1 + items.length) % items.length;
+    }
+
+    /**
+     *
+     * @param capacity
+     */
+    private void resize(int capacity){
+        T[] newitems = (T[]) new Object[capacity];
+
+//        int cur = head;
+//        for(int i = 0; i < size; i++){
+//            newitems[i] = items[cur];
+//            cur = getNext(head);
+//        }
+//
+//        head = 0;
+//        tail = size;
+        if(head <  tail){
+            System.arraycopy(items, head, newitems, 0, size);
+        } else if (size > 0) {
+            int rightpartLength = items.length - head;
+            System.arraycopy(items, head, newitems, 0, rightpartLength);
+
+            int leftpartLength = tail;
+            System.arraycopy(items, 0, newitems, rightpartLength, leftpartLength);
+        }
+        items = newitems;
+        head = 0;
+        tail = size;
+    }
+
+    private void checkAndGrow() {
+        if (size == items.length) {
+            resize(items.length * RESIZE_FACTOR);
+        }
+    }
+
+    private void checkAndShrink() {
+//        if ((double) size / items.length < MIN_USAGE_RATIO) {
+        if (items.length >= 16 && (double) size / items.length < MIN_USAGE_RATIO) {
+            resize(items.length / RESIZE_FACTOR);
+        }
+    }
+
+    public void addFirst(T item){
+//        if(size == items.length){
+//            resize(items.length * RESIZE_FACTOR);
+//        }
+        checkAndGrow();
+
+//        items[head] = item;
+//        head = getPrev(head);
+        head = getPrev(head);
+        items[head] = item;
+        size++;
+    }
+
+    public void addLast(T item){
+//        if(size == items.length){
+//            resize(items.length * RESIZE_FACTOR);
+//        }
+        checkAndGrow();
+
+//        tail = getNext(tail);
+//        items[tail] = item;
+        items[tail] = item;
+        tail = getNext(tail);
+        size++;
+    }
+
+    public T removeFirst(){
+        if(size == 0){
+            return null;
+        }
+        T item = items[head];
+        items[head] = null;
+        head = getNext(head);
+        size--;
+//        return item;
+
+//        if((double) size / items.length < MIN_USAGE_RATIO){
+//            resize(items.length / RESIZE_FACTOR);
+//        }
+        checkAndShrink();
+        return item;
+    }
+
+    public T removeLast(){
+        if(size == 0){
+            return null;
+        }
+
+        int index = getPrev(tail);
+        T item = items[index];
+        items[index] = null;
+        tail = index;
+        size--;
+
+//        if((double) size / items.length < MIN_USAGE_RATIO){
+//            resize(items.length / RESIZE_FACTOR);
+//        }
+        checkAndShrink();
+        return item;
+    }
+
+    public T get(int index){
+        if(index < 0 || index >= size){
+            return null;
+        }
+
+//        int physicalIndex = (head + index) % size;
+        int physicalIndex = (head + index) % items.length;
+        return items[physicalIndex];
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public void printDeque(){
+        for(int i = 0; i < size; i++){
+            System.out.print(get(i) + " ");
+        }
+
+        System.out.println();
+    }
+
+    public ArrayDeque(ArrayDeque<T> other){
+        int capacity = Math.max(INITIAL_CAPACITY, other.size);
+
+//        this.items = (T[]) new Object[INITIAL_CAPACITY];
+        this.items = (T[]) new Object[capacity];
+        this.size = other.size;
+
+        if(other.head < other.tail){
+            System.arraycopy(other.items, other.head, this.items, 0, other.size);
+        } else if (other.size > 0) {
+            int rightpartLength = other.items.length - other.head;
+            System.arraycopy(other.items, other.head, this.items, 0, rightpartLength);
+
+            int leftpartLength = other.tail;
+            System.arraycopy(other.items, 0, this.items, rightpartLength, leftpartLength);
+        }
+        head = 0;
+        tail = size;
+    }
 }
+
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,4 +1,184 @@
-package PACKAGE_NAME;
+/**
+ * 一个使用循环哨兵实现的泛型双端队列。
+ * @param <T> 队列中存储的元素类型。
+ * */
+public class LinkedListDeque<T> {
 
-public class LinkedListDeque {
+    /**
+     * 内部泛型节点类。
+     * @param <T> 节点存储的元素类型。
+     */
+//    private static class IntNode{
+    private static class Node<T> {
+        public Node<T>  prev;
+        public Node<T>  next;
+        public T item;
+
+        //    public IntNode(IntNode p, int i, IntNode n){
+        public Node(Node<T> p, T i, Node<T> n) {
+            this.prev = p;
+            this.next = n;
+            this.item = i;
+        }
+    }
+
+    private Node<T> sentinel;
+    private int size;
+
+    /**
+     * 创建一个包含单个元素的双端队列。
+     * @param x 要添加的初始元素。
+     */
+    public LinkedListDeque(T x){
+        sentinel = new Node<>(null,null ,null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+
+        Node<T> newNode = new Node<>(sentinel,x,sentinel);
+        sentinel.next = newNode;
+        sentinel.prev = newNode;
+        size = 1;
+    }
+
+    /**
+     * 创建一个空的双端队列。
+     */
+    public LinkedListDeque(){
+        sentinel = new Node<>(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
+    }
+
+    /**
+     * 在队列前端添加一个元素。
+     * @param x 要添加的元素。
+     */
+    public void addFirst(T x){
+        Node<T> newNode = new Node<>(sentinel,x,sentinel.next);
+        sentinel.next.prev = newNode;
+        sentinel.next = newNode;
+        size++;
+    }
+
+    /**
+     * 在队列后端添加一个元素。
+     * @param x 要添加的元素。
+     */
+    public void addLast(T x){
+        Node<T> newnode = new Node<>(sentinel.prev, x, sentinel);
+        sentinel.prev.next = newnode;
+        sentinel.prev = newnode;
+        size++;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    /**
+     * 打印队列中的所有元素，从头到尾，以空格分隔。
+     * 打印完所有元素后换行。
+     */
+    public void printDeque(){
+        Node<T> cur = sentinel.next;
+        while(cur != sentinel){
+            System.out.print(cur.item + " ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+
+    /**
+     * 移除并返回双端队列前端的元素。
+     * 若不存在该元素，则返回 null。
+     */
+    public T removeFirst(){
+        Node<T> cur = sentinel.next;
+        if(cur == sentinel){
+            return null;
+        }
+        sentinel.next = cur.next;
+        cur.next.prev = sentinel;
+        size--;
+        return cur.item;
+    }
+
+    /**
+     * 移除并返回双端队列末端的元素。
+     * 若不存在该元素，则返回 null。
+     */
+    public T removeLast(){
+        Node<T> cur = sentinel.prev;
+        if(cur == sentinel){
+            return null;
+        }
+//        sentinel.prev = cur.prev.next;
+        sentinel.prev = cur.prev;
+        cur.prev.next = sentinel;
+        size--;
+        return cur.item;
+    }
+
+    /**
+     * Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+     * If no such item exists, returns null. Must not alter the deque
+     */
+    public T get(int index){
+        Node<T> cur = sentinel.next;
+        if(index < 0 || index >= size){
+            return null;
+        }
+        for(int i = 0; i < index; i++){
+            cur = cur.next;
+        }
+        return cur.item;
+    }
+
+    /**
+     * Same as get, but uses recursion.
+     * @param index
+     * @return
+     */
+    public T getRecursive(int index){
+        if(index < 0 || index >= size){
+            return null;
+        }
+        return getRecursiveHelper(sentinel.next, index);
+    }
+
+    /**
+     * 私有的递归辅助方法。
+     * @param cur
+     * @param index
+     * @return
+     */
+    private T getRecursiveHelper(Node<T> cur, int index){
+        if(index == 0) return cur.item;
+        return getRecursiveHelper(cur.next, index-1);
+    }
+
+    public int size(){
+        return size;
+    }
+
+    /**
+     * Creates a deep copy of other
+     * means that you create an entirely new ArrayDeque, with the exact same items as other.
+     * However, they should be different objects,
+     * if you change other, the new ArrayDeque you created should not change as well.
+     * @param other
+     */
+    public LinkedListDeque(LinkedListDeque<T> other){
+        sentinel = new Node<>(null,null,null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
+
+        Node<T> p =other.sentinel.next;
+        while(p != other.sentinel){
+            this.addLast(p.item);
+            p = p.next;
+        }
+    }
 }
